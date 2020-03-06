fac4998f6fdc45f98966fafb3d1b6ed2a7b43450
Tim Veil <3260845+timveil@users.noreply.github.com>
2020-03-06 10:11:14 -0500

    Initial commit

---
+++ jdbc-template/src/main/java/io/crdb/spring/JdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package io.crdb.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcTemplateApplication.class, args);
+	}
+
+}
