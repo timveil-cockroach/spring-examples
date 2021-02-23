b11d005ee85a5a74da2f98e6ab8d601266368691
Tim Veil <3260845+timveil@users.noreply.github.com>
2021-02-23 09:35:40 -0500

    adding reactive example

---
+++ reactive/src/main/java/io/crdb/spring/ReactiveApplication.java
@@ -0,0 +1,84 @@
+package io.crdb.spring;
+
+import com.github.javafaker.Faker;
+import io.r2dbc.spi.ConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Locale;
+
+@SpringBootApplication
+public class ReactiveApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ReactiveApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApplication.class, args);
+    }
+
+    @Bean
+    public Faker faker() {
+        return new Faker(Locale.US);
+    }
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+
+        return initializer;
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+
+        return (args) -> {
+            // save a few customers
+            repository.saveAll(Arrays.asList(new Customer("Jack", "Bauer"),
+                    new Customer("Chloe", "O'Brian"),
+                    new Customer("Kim", "Bauer"),
+                    new Customer("David", "Palmer"),
+                    new Customer("Michelle", "Dessler")))
+                    .blockLast(Duration.ofSeconds(10));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().doOnNext(customer -> {
+                log.info(customer.toString());
+            }).blockLast(Duration.ofSeconds(10));
+
+            log.info("");
+
+            // fetch an individual customer by ID
+            repository.findById(1L).doOnNext(customer -> {
+                log.info("Customer found with findById(1L):");
+                log.info("--------------------------------");
+                log.info(customer.toString());
+                log.info("");
+            }).block(Duration.ofSeconds(10));
+
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            repository.findByLastName("Bauer").doOnNext(bauer -> {
+                log.info(bauer.toString());
+            }).blockLast(Duration.ofSeconds(10));
+            ;
+            log.info("");
+        };
+    }
+
+}
